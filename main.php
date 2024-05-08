dd0995645fa9aed372232d5eae669e6701c88983
Seyyed_Amirreza <amir9bahoosh@gmail.com>
2024-05-08 21:36:44 +0330

    Flow Control

--- main.php
+++ main.php
@@ -8,26 +8,62 @@
 </head>
 <body>
     <?php
-    $num = 5;
+    $num = 10;
+    $temp = 0;
     if($num < 10){
-        echo "input is < 10<br>";
-    } elseif($num > 10){
-        echo 'input is > 10<br>';
-    }else{
-        echo " your input is 10<br>";
+        echo "input is < 10";
+    }elseif($num > 10){
+        echo 'input is > 10';
     }
-    switch($num){
-        case 10:
-            echo "Mid";
+    //اگر عدد برابر 10 بود،آن عدد را 10 بار در خودش جمع کن
+    else{
+        for($i = 0;$i <= 9;$i++){
+            $temp += $num;  
+        }
+        echo "$temp";
+    }
+    echo "<br>";
+    //ماه های میلادی
+    $month = "February";
+    switch($month){
+        case "January":
+            echo "First month";
             break;
-        case 20 :
-            echo "Big";
+        case "February" :
+            echo "Second month";
             break;
-        case 5:
-            echo "Small";
+        case "March":
+            echo "Third month";
+            break;
+        case "April":
+            echo "Fourth month";
+            break;
+        case "May":
+            echo "Fifth month";
+            break;
+        case "June":
+            echo "Sixth month";
+            break;
+        case "July":
+            echo "Seventh month";
+            break;
+        case "August":
+            echo "Eighth month";
+            break;
+        case "September":
+            echo "Ninth month";
+            break;
+        case "October":
+            echo "Tenth month";
+            break;
+        case "November":
+            echo "Eleventh month";
+            break;
+        case "December":
+            echo "Twelfth month";
             break;
         default:
-            echo "None";
+            echo "Invalid Input";
     }
     ?>
 </body>
