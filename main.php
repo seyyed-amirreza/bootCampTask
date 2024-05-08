ec9831a893b1c9561cf0077bb1627f5f160e258c
Seyyed_Amirreza <amir9bahoosh@gmail.com>
2024-05-08 15:52:01 +0330

    Updated Flow Control

--- main.php
+++ main.php
@@ -8,13 +8,26 @@
 </head>
 <body>
     <?php
-    $num = 20;
+    $num = 5;
     if($num < 10){
-        echo "input is < 10";
+        echo "input is < 10<br>";
     } elseif($num > 10){
-        echo 'input is > 10';
+        echo 'input is > 10<br>';
     }else{
-        echo " your input is 10";
+        echo " your input is 10<br>";
+    }
+    switch($num){
+        case 10:
+            echo "Mid";
+            break;
+        case 20 :
+            echo "Big";
+            break;
+        case 5:
+            echo "Small";
+            break;
+        default:
+            echo "None";
     }
     ?>
 </body>
