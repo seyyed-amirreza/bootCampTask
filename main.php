90e09a3e3bdda8b1f88b234c2be7992da3b85ea6
Seyyed_Amirreza <amir9bahoosh@gmail.com>
2024-05-08 15:40:11 +0330

    Flow Control

---
+++ main.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $num = 20;
+    if($num < 10){
+        echo "input is < 10";
+    } elseif($num > 10){
+        echo 'input is > 10';
+    }else{
+        echo " your input is 10";
+    }
+    ?>
+</body>
+</html>
